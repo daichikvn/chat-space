539455611af76946a8a0636a8b4a83d8d9c50f55
daichikvn <daichikvn7000@gmail.com>
2019-09-19 03:14:12 +0900

    automatic updating implementation

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,8 @@
 $(document).on('turbolinks:load', function(){
   function buildHTML(message){
-    var img = message.image ? `<img src= ${ message.image } class="message-image">` : "";
+    var img = message.image.url ? `<img src= ${ message.image.url } class="message-image">` : "";
     
-    var html = `<div class="message">
+    var html = `<div class="message" data-id="${message.id}">
                   <div class="message__upper-info">
                     <div class="message__upper-info--talker">
                       ${message.user_name}
@@ -35,7 +35,7 @@ $(document).on('turbolinks:load', function(){
       var html = buildHTML(data);
       $('.message-box').append(html)
       $('#new_message')[0].reset();
-      $('.main-body').animate({ scrollTop: $('.main-body')[0].scrollHeight });
+      $('.main-body').animate({ scrollTop: $('.main-body')[0].scrollHeight }, 'fast');
       $('.send-btn').prop("disabled", false);
     })
     .fail(function(data){
@@ -43,5 +43,29 @@ $(document).on('turbolinks:load', function(){
       $('.send-btn').prop("disabled", false);
     })
   })
+
+  var reloadMessages = function() {
+    if (location.href.match(/\/groups\/\d+\/messages/)){
+      var last_message_id = $('.message:last').data('id');
+      $.ajax({
+        url: "api/messages",
+        type: 'get',
+        dataType: 'json',
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        var insertHTML = '';
+        messages.forEach(function (message) {
+        insertHTML = buildHTML(message);
+        $('.message-box').append(insertHTML)
+        $('.main-body').animate({ scrollTop: $('.main-body')[0].scrollHeight }, 'fast');
+        })
+      })
+      .fail(function() {
+        alert('error');
+      });
+    }
+  };
+  setInterval(reloadMessages, 5000);
 })
 
